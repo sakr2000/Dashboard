222c1fdc34d091433ae93f24be3ed178ce0886cf
mohamed_sakr <sakr83117@gmail.com>
2023-10-27 22:53:22 +0200

    initial setup

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+@NgModule({
+  declarations: [SidenavComponent],
+  imports: [CommonModule],
+  exports: [SidenavComponent],
+})
+export class SharedModule {}
